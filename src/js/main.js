182c23377f3d07208ac333755aa04233b47c02c2
Stephanie Merle <merle.stephany@gmail.com>
2019-10-30 19:25:48 +0100

    with dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
 console.log("it works");
 const hamburger = document.querySelector("#hamburger");
 const drawer = document.querySelector("#myDrawer");
+const mode = document.querySelector("#mode");
+const body = document.querySelector("body");
+const collec = document.querySelector("#collection");
+const title = document.querySelector("h1");
+const sticker = document.querySelector("#sticker");
+const nav = document.querySelector("nav");
+
 hamburger.addEventListener("click", () => {
   drawer.classList.toggle("menu");
+});
+
+mode.addEventListener("click", () => {
+  console.log("clicked");
+  body.classList.toggle("dark");
+  title.classList.toggle("dark");
+  sticker.classList.toggle("dark");
+  nav.classList.toggle("darknav");
 });
