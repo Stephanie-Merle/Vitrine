5149aab457836d3923d17b8a7f86c58f2f2b1a97
Stephanie Merle <merle.stephany@gmail.com>
2019-10-31 22:59:01 +0100

    cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,6 @@ const hamburger = document.querySelector("#hamburger");
 const drawer = document.querySelector("#myDrawer");
 const mode = document.querySelector("#mode");
 const body = document.querySelector("body");
-const collec = document.querySelector("#collection");
-const title = document.querySelector("h1");
 const sticker = document.querySelector("#sticker");
 const nav = document.querySelector("nav");
 
@@ -13,7 +11,6 @@ hamburger.addEventListener("click", () => {
 });
 
 mode.addEventListener("click", () => {
-  console.log("clicked");
   body.classList.toggle("dark");
   title.classList.toggle("dark");
   sticker.classList.toggle("dark");
